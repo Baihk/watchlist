98c6938d866abfc2c85076c612c3b4b7967a0342
Bai <hkbai@ruc.edu.cn>
2023-12-04 20:13:13 +0800

    Ready to deploy

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask_login import login_user, login_required, current_user, logout_user
 import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/白颢/Documents/白浩坤/学习资料/大三上/数据库系统概论/watchlist/data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 
 
@@ -134,6 +134,9 @@ def index():
 @app.errorhandler(404)  # 传入要处理的错误代码
 def page_not_found(e):  # 接受异常对象作为参数
     return render_template('404.html'), 404  # 返回模板和状态码
+
+
+
 
 @app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
 @login_required
