3ce834701de1abfed144fc168435413463f1340d
Bai <hkbai@ruc.edu.cn>
2023-12-04 21:52:25 +0800

    Ready to deploy

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ from flask_login import LoginManager, UserMixin
 from flask_login import login_user, login_required, current_user, logout_user
 
 import os
+import sys
+
+# SQLite URI compatible
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
